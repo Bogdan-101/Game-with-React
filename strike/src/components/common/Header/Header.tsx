0f97829ab7c7742f774170a6faeab4e320fd0c8d
Bogdan Karmyzow <big_bogdan@rambler.ru>
2021-06-28 13:47:21 +0300

    removed unintentioned commit

--- strike/src/components/common/Header/Header.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './Header.css'
-
-export const Header: React.FC = () => {
-  return <header>header</header>
-}
--- strike/src/components/pages/Battle/Battle.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './Battle.css'
-
-export const Battle: React.FC = () => {
-  return <div className="battle__wrapper">Battle</div>
-}
